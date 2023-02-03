3eef7a4af347607d136206b79c113a9d6515c1ad
Jack C <jcanducci@gmail.com>
2023-02-03 17:00:28 -0500

    Updated AnemometerArduino code to measure wind speed and put in calls to adjust rotation of system

--- AnemometerArduino.cpp
+++ AnemometerArduino.cpp
@@ -1,3 +1,8 @@
+// Wind Rate Variables
+const int MAX_WIND_RATE_KNOTS = 35;
+const int MIN_WIND_RETURN_RATE_KNOTS = 30;
+int windRateCounter = 0;
+bool flatModeTriggered = 0;
 
 void setup()
 {
@@ -10,32 +15,72 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void Anemometer()
+float getWindSpeed()
 {
     float sensorValue = analogRead(A0);
-    Serial.print("Analog Value =");
-    Serial.println(sensorValue);
+    // Serial.print("Analog Value =");
+    // Serial.println(sensorValue);
 
     float voltage = (sensorValue / 1024) * 5; //Arduino ADC resolution 0-1023
-    Serial.print("Voltage =");
-    Serial.print(voltage);
-    Serial.println(" V");
+    // Serial.print("Voltage =");
+    // Serial.print(voltage);
+    // Serial.println(" V");
 
-    float wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
-    float speed_mph = ((wind_speed *3600)/1609.344);
-    float speed_knot = ((wind_speed * 1.943844));
-    Serial.print("Wind Speed =");
-    Serial.print(wind_speed);
-    Serial.println("m/s");
-    Serial.print(speed_mph);
-    Serial.println("mph");
-    Serial.print(speed_knot);
-    Serial.println("knots");
+    return mapfloat(voltage, 0.4, 2, 0, 32.4);
+}
 
-    Serial.println(" ");
-    delay(1000);
+void sendFlatMode()
+{
+    // send signal to actuator to go flat
 }
-void loop() {
-    // put your main code here, to run repeatedly:
-    Anemometer();
+
+void sendNormalMode()
+{
+    // send signal to go back to normal operations
 }
+
+void loop()
+{
+    float windSpeed = 0.0f;
+    float speedKnots = 0.0f;
+
+    windSpeed = getWindSpeed();
+
+    speedKnots = windSpeed * 1.943844;
+
+    // if we're currently in recovery mode or not
+    if (flatModeTriggered)
+    {
+        // if we are at our 10m passed at wind rate, return to normal
+        if (windRateCounter >= 10)
+        {
+            windRateCounter = 0;
+            flatModeTriggered = false;
+            sendNormalMode();
+        }
+        // if we're currently above the wind rate, log it and delay for 1s
+        else if (speedKnots < MIN_WIND_RETURN_RATE_KNOTS)
+        {
+            windRateCounter++;
+            delay(60000);
+        }
+    }
+    else
+    {
+        // if we are at our 20s passed at max wind rate, trigger flatmode
+        if (windRateCounter >= 20)
+        {
+            windRateCounter = 0;
+            flatModeTriggered = true;
+            sendFlatMode();
+        }
+        // if we're currently above the wind rate, log it and delay for 1s
+        else if (speedKnots > MAX_WIND_RATE_KNOTS)
+        {
+            windRateCounter++;
+            delay(1000);
+        }
+    }
+}
+// <30 10m 
+// >35 20s
